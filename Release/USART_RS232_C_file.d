6f33a647407ce6467bb30150f3bc198ee82015ba
marwis95 <marwis95@o2.pl>
2020-11-04 20:57:37 +0100

    Init

---
+++ Release/USART_RS232_C_file.d
@@ -0,0 +1,3 @@
+Release/USART_RS232_C_file.o: USART_RS232_C_file.c USART_RS232_H_file.h
+
+USART_RS232_H_file.h:
---
+++ Release/main.d
@@ -0,0 +1,5 @@
+Release/main.o: main.c USART_RS232_H_file.h hd44780.h
+
+USART_RS232_H_file.h:
+
+hd44780.h:
